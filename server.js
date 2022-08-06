4d6dc43da8006f901e011ec0bbaa1eaaff86dfd6
Sam-ONeill <samoneill500@gmail..com>
2022-08-06 22:27:32 +0100

    Adding socketIO to heroku server

--- server.js
+++ server.js
@@ -29,10 +29,10 @@ app.use('/groups', groupRouter);
 const io = SocketIO(server);
 
 //socketIO functions
-function getActiveRooms(io) {
+function getActiveRooms(socket) {
   // Convert map into 2D list:
   // ==> [['4ziBKG9XFS06NdtVAAAH', Set(1)], ['room1', Set(2)], ...]
-  const arr = Array.from(io.sockets.adapter.rooms);
+  const arr = Array.from(socket.adapter.rooms);
   // Filter rooms whose name exist in set:
   // ==> [['room1', Set(2)], ['room2', Set(2)]]
   const filtered = arr.filter(room => !room[1].has(room[0]))
