0dfad624f3d5d608fdf9dd5c156310a1b218793a
Sam-ONeill <samoneill500@gmail..com>
2022-08-10 20:28:24 +0100

    Adding socketIO to heroku server

--- server.js
+++ server.js
@@ -37,7 +37,6 @@ let socketUserName = ""
 //Begin SocketIO init
 
 const io = SocketIO(server);
-io.disconnectSockets();
 //socketIO functions
 
 
@@ -46,29 +45,24 @@ io.disconnectSockets();
 //General functions on startup
 
 io.use((socket, next) => {
-    const sessionID = socket.handshake.auth.sessionID;
-    console.log("is tehre a socekt? " + socket);
+    const sessionID = socket.sessionID;
+    console.log("is tehre a socekt? " + socket+" "+ sessionID);
     if (sessionID) {
         // find existing session
         const session = sessionStore.findSession(sessionID);
         if (session) {
             socket.sessionID = sessionID;
             socket.userID = session.userID;
-            socket.username = session.username;
             return next();
         }
     }
-    const username = socket.handshake.auth.username;
-    console.log("username " + username);
-    if (!username) {
-        return next(new Error("invalid username"));
-    }
-    console.log("created new session");
+
 
     // create new session
     socket.sessionID = randomId();
     socket.userID = randomId();
-    socket.username = username;
+    console.log("userid " + socekt.userID);
+    console.log("created new session");
     next();
 
 });
